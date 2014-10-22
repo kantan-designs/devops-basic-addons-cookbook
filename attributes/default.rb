7d25d3f6e93fa83dc7fade2916da001a1df2c5a9
John McDowall <john@mcdowall.info>
2014-10-21 19:35:42 -0700

    Update locale

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 # default environment flag (not linked to rails environment)
 default[:environment] = 'development'
-default["devops-basic-addons-cookbook"]["additional_locales"] = []
+default["devops-basic-addons-cookbook"]["additional_locales"] = ['en_CA.UTF-8']
